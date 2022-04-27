179459b7dafe0fc00ddf910f74e72379b53c6790
Albetb <aalbetb@hotmail.it>
2022-04-27 11:38:33 +0200

    0.1.0 - WEB
    Started building a web app for better interfaces
    Note that not all items on the page are correctly implemented

--- init.py
+++ init.py
@@ -1,13 +1,44 @@
 from shop import Shop
+from flask import Flask, render_template, url_for, request, redirect
+
+app = Flask(__name__)
 
 mario = Shop(
                 "Mario",
                 city_level = 3,
-                party_level = 5,
-                shop_level = 5,
+                party_level = 20,
+                shop_level = 10,
                 reputation = 0,
-                template = "Mage Merchant"
+                template = "Magic Blacksmith"
             )
 
 mario.generate_inventory()
-mario.display(2)
+# mario.display(2)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        return render_template('index.html', items = mario.stock)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    try:
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'POST':
+        try:
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
