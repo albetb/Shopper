ceeb6dcc7649506118693912cd31ca61fffad7c0
Albetb <albetb@hotmail.it>
2024-05-17 00:15:49 +0200

    qol create component

--- src/components/common/create_component.js
+++ src/components/common/create_component.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
-import '../../style/menu_cards.css';
+import React, { useState, useRef, useEffect } from 'react';
 
 const CreateComponent = ({ props }) => {
   const [name, setName] = useState('');
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   const handleOkClick = () => {
-    props.onNew(name)
+    props.onNew(name);
     props.setIsVisible(false);
     setName('');
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleOkClick();
+    }
   };
 
   const placeholder = () => {
@@ -15,18 +27,19 @@ const CreateComponent = ({ props }) => {
   };
 
   return (
-    <>
-      <div className='card-side-div'>
-        <input className='modern-dropdown'
-          type='text'
-          placeholder={placeholder()}
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button className='modern-button' onClick={handleOkClick}>Ok</button>
-      </div>
-    </>
+    <div className='card-side-div'>
+      <input
+        ref={inputRef}
+        className='modern-dropdown'
+        type='text'
+        placeholder={placeholder()}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button className='modern-button' onClick={handleOkClick}>Ok</button>
+    </div>
   );
-}
+};
 
 export default CreateComponent;
