763a6edf2d8652a497f05b34680b027f81bb34a9
Albetb <albetb@hotmail.it>
2024-05-17 01:32:32 +0200

    Added saving data functionality

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import MenuCards from '../menu_cards/menu_cards';
+import { handleFileUpload, downloadLocalStorage } from '../../lib/storage'
 import '../../style/sidebar.css';
 
 const Sidebar = ({ props }) => {
+  const handleUploadClick = () => {
+    const fileInput = document.getElementById('upload');
+    fileInput.click();
+  };
+
   return (
     <div>
       <div className={`sidebar ${props.isSidebarCollapsed ? 'collapsed' : ''}`}>
@@ -16,12 +22,29 @@ const Sidebar = ({ props }) => {
 
         {!props.isSidebarCollapsed && (
           <div className='content'>
+            <div className='sidebar-buttons'>
+
+              <button className='modern-button' style={{ marginLeft: '5px', marginBottom: '5px' }} onClick={downloadLocalStorage}>
+                Save Data
+              </button>
+
+              <input
+                type='file'
+                id='upload'
+                style={{ display: 'none' }}
+                accept='application/json'
+                onChange={handleFileUpload}
+              />
+              <button className='modern-button' style={{ marginLeft: '15px', marginBottom: '5px' }} onClick={handleUploadClick}>
+                Upload Data
+              </button>
+
+            </div>
+
             <MenuCards props={props} />
           </div>
         )}
-
       </div>
-
     </div>
   );
 };
--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,3 +1,4 @@
+import { compressToUTF16, decompressFromUTF16 } from 'lz-string';
 import World from './world';
 import City from './city';
 import Shop from './shop';
@@ -5,23 +6,28 @@ import Shop from './shop';
 //#region get
 
 export function getWorlds() {
-    return JSON.parse(localStorage.getItem('Worlds')) || [];
+    const value = localStorage.getItem('Worlds');
+    return value ? JSON.parse(decompressFromUTF16(value)) : [];
 }
 
 export function getSelectedWorld() {
-    return JSON.parse(localStorage.getItem('SelectedWorld')) || { Id: null, Name: null };
+    const value = localStorage.getItem('SelectedWorld');
+    return value ? JSON.parse(decompressFromUTF16(value)) : { Id: null, Name: null };
 }
 
 export function getWorld(id) {
-    return new World().load(JSON.parse(localStorage.getItem(`World/${id}`)));
+    const value = localStorage.getItem(`World/${id}`);
+    return value ? new World().load(JSON.parse(decompressFromUTF16(value))) : null;
 }
 
 export function getCity(id) {
-    return new City().load(JSON.parse(localStorage.getItem(`City/${id}`)));
+    const value = localStorage.getItem(`City/${id}`);
+    return value ? new City().load(JSON.parse(decompressFromUTF16(value))) : null;
 }
 
 export function getShop(id) {
-    return new Shop().load(JSON.parse(localStorage.getItem(`Shop/${id}`)));
+    const value = localStorage.getItem(`Shop/${id}`);
+    return value ? new Shop().load(JSON.parse(decompressFromUTF16(value))) : null;
 }
 
 //#endregion
@@ -29,23 +35,64 @@ export function getShop(id) {
 //#region set
 
 export function setWorlds(value) {
-    localStorage.setItem('Worlds', JSON.stringify(value));
+    localStorage.setItem('Worlds', compressToUTF16(JSON.stringify(value)));
 }
 
 export function setSelectedWorld(value) {
-    localStorage.setItem('SelectedWorld', JSON.stringify(value));
+    localStorage.setItem('SelectedWorld', compressToUTF16(JSON.stringify(value)));
 }
 
 export function setWorld(value) {
-    localStorage.setItem(`World/${value.Id}`, JSON.stringify(value));
+    localStorage.setItem(`World/${value.Id}`, compressToUTF16(JSON.stringify(value)));
 }
 
 export function setCity(value) {
-    localStorage.setItem(`City/${value.Id}`, JSON.stringify(value));
+    localStorage.setItem(`City/${value.Id}`, compressToUTF16(JSON.stringify(value)));
 }
 
 export function setShop(value) {
-    localStorage.setItem(`Shop/${value.Id}`, JSON.stringify(value));
+    localStorage.setItem(`Shop/${value.Id}`, compressToUTF16(JSON.stringify(value)));
 }
 
 //#endregion
+
+const getAllLocalStorageItems = () => {
+    const items = {};
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const compressedValue = localStorage.getItem(key);
+        if (compressedValue) {
+            items[key] = decompressFromUTF16(compressedValue);
+        }
+    }
+    return JSON.stringify(items, null, 2);
+};
+
+export const downloadLocalStorage = () => {
+    const data = getAllLocalStorageItems();
+    const blob = new Blob([data], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'ShopperStorageData.json';
+    a.click();
+    URL.revokeObjectURL(url);
+};
+
+export const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        try {
+            const data = JSON.parse(e.target.result);
+            localStorage.clear();
+            for (const key in data) {
+                localStorage.setItem(key, compressToUTF16(data[key]));
+            }
+            window.location.reload();
+        } catch (error) { }
+    };
+
+    reader.readAsText(file);
+};
