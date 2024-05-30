394438d495ee9af7278a1c14dcb0132cb5103ac7
Albetb <albetb@hotmail.it>
2024-05-30 12:13:02 +0200

    Updated border-radius for all element

--- src/components/shop_inventory/delete_popup.js
+++ src/components/shop_inventory/delete_popup.js
@@ -39,7 +39,7 @@ const DeletePopup = ({ itemName, itemType, itemNumber, onClose, onDelete, positi
                     onChange={(e) => setNum(e.target.value)}
                     min='0'
                     max={itemNumber}
-                    className='modern-input small no-padding'
+                    className='small-scrollbar no-padding'
                 />
                 <div className='level-frame'>
 
@@ -48,13 +48,13 @@ const DeletePopup = ({ itemName, itemType, itemNumber, onClose, onDelete, positi
             </div>
 
             <div className='card-side-div margin-top width-90'>
-                <button className='item-number-button small' onClick={handleDelete}>
+                <button className='item-number-button medium' onClick={handleDelete}>
                     <span className='material-symbols-outlined'>
                         remove_shopping_cart
                     </span>
                 </button>
 
-                <button className='item-number-button small' onClick={onClose}>
+                <button className='item-number-button medium' onClick={onClose}>
                     <span className='material-symbols-outlined'>
                         close
                     </span>
