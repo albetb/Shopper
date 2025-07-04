5817c412b106278a6612aeb307c246a1786623a2
Albetb <albetb@hotmail.it>
2025-07-04 10:16:01 +0200

    Updated main page and defined colors in css

--- src/components/main_page/main_page.js
+++ src/components/main_page/main_page.js
@@ -1,47 +1,13 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import '../../style/shop_inventory.css';
 import { setStateCurrentTab } from '../../store/slices/appSlice';
 import { isMobile } from '../../lib/utils';
 
 export default function MainPage() {
   const dispatch = useDispatch();
-  const [showAddItemForm, setShowAddItemForm] = useState(false);
 
   const handleShopClick = () => dispatch(setStateCurrentTab(1));;
   const handleSpellbookClick = () => dispatch(setStateCurrentTab(2));
-
-  const shopButton =
-    <button
-      className={`modern-dropdown small`}
-      onClick={handleShopClick}
-      title="Shop generator"
-    >
-      <span className="material-symbols-outlined">shopping_cart</span>
-    </button>
-
-  const spellbookButton =
-    <button
-      className={`modern-dropdown small`}
-      onClick={handleSpellbookClick}
-      title="Spellbook"
-    >
-      <span className="material-symbols-outlined">menu_book</span>
-    </button>
-
-
-  const buttons = (
-    <>
-      <div className="menu-side-by-side">
-        <p style={{ textShadow: "1px 1px #12121366" }}>Shop generator</p>
-        {shopButton}
-      </div>
-      <div className="menu-side-by-side">
-        <p style={{ textShadow: "1px 1px #12121366" }}>Spellbook</p>
-        {spellbookButton}
-      </div>
-    </>
-  );
 
   const isMobileNow = isMobile();
   const ww = isMobileNow ? "80%" : "20%";
@@ -51,21 +17,27 @@ export default function MainPage() {
   return (
     <>
       <h1>Welcome to Shopperino!</h1>
-      <p style={{ color: "#d5d5d5", width: "90%", textAlign: "center" }}>A collection of tools for Dungeons and Dragons 3.5</p>
-      <div className="card" style={{ width: ww, padding: pp, paddingLeft: plr, paddingRight: plr }}>
-      <div className="menu-side-by-side">
-        <p style={{ textShadow: "1px 1px #12121366" }}>Shop generator</p>
-        {shopButton}
-      </div>
+
+      <p style={{ color: "#f9f9f9", width: "90%", textAlign: "center" }}>A collection of tools for Dungeons and Dragons 3.5</p>
+
+      <div className="card" style={{ width: ww, padding: pp, paddingLeft: plr, paddingRight: plr, cursor: "pointer" }} onClick={handleShopClick}>
+        <p style={{ textShadow: "1px 1px #0d0d0d1a" }}>Shop generator</p>
+        <span className="material-symbols-outlined" style={{ color: "#0d0d0d" }}>shopping_cart</span>
         <p style={{ textAlign: "center" }}>Generates randomized shops whose inventory dynamically scales to the player’s level.</p>
       </div>
-      <div className="card" style={{ width: ww, padding: pp, paddingLeft: plr, paddingRight: plr }}>
-      <div className="menu-side-by-side">
-        <p style={{ textShadow: "1px 1px #12121366" }}>Spellbook</p>
-        {spellbookButton}
+
+      <div className="card" style={{ width: ww, padding: pp, paddingLeft: plr, paddingRight: plr, cursor: "pointer" }} onClick={handleSpellbookClick}>
+        <p style={{ textShadow: "1px 1px #0d0d0d1a" }}>Spellbook</p>
+        <span className="material-symbols-outlined" style={{ color: "#0d0d0d" }}>menu_book</span>
+        <p style={{ textAlign: "center" }}>A spellbook that lets players organize and track their learned spells.</p>
       </div>
-        <p style={{ textAlign: "center" }}>An spellbook that lets players organize and track their learned spells.</p>
-      </div>
+
+      <p style={{ color: "#f9f9f9", width: "90%", textAlign: "center" }}>
+        If you encounter any bugs or inaccurate descriptions, please report them on our{" "}
+        <a href="https://github.com/albetb/Shopperino/issues" target="_blank" rel="noopener noreferrer" style={{ color: "#f9f9f9", textDecoration: "underline" }}>
+          GitHub issues page
+        </a>.
+      </p>
     </>
   );
 }
